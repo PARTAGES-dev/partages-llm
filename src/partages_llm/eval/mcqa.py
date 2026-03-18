d9ff1c583434237b1aadfa5bf8aff2084a72d208
a-mannion <aidan.mannion@outlook.com>
2026-03-18 19:12:33 +0100

    refactor for readability/modularity

--- src/partages_llm/eval/mcqa.py
+++ src/partages_llm/eval/mcqa.py
@@ -1,11 +1,97 @@
 import re
-from typing import Dict, Optional, Union
+from typing import Any, Dict, List, Optional, Set, Tuple, Union
 from tqdm import tqdm
 from collections import defaultdict
 
 from datasets import Dataset
 from peft import PeftModelForCausalLM
-from transformers import PreTrainedModel, PreTrainedTokenizerFast
+from transformers import BatchEncoding, PreTrainedModel, PreTrainedTokenizerFast
+
+
+def _prepare_dataset(dataset: Dataset, batch_size: int) -> Dataset:
+    if "seq_len" in dataset.features:
+        dataset_to_batch = dataset
+    else:
+        dataset_to_batch = dataset.map(
+            lambda x: {"seq_len": len(x["input_ids"])},
+            desc="Counting tokens",
+        ).sort("seq_len", reverse=False)
+    prepared_dataset = dataset_to_batch.batch(batch_size)
+    return prepared_dataset
+
+
+def _prepare_batch(
+    batch: Dict[str, List[int]],
+    keys: List[str],
+    tokenizer: PreTrainedTokenizerFast,
+    device: str
+) -> BatchEncoding:
+    batch_model_input = {k: v for k, v in batch.items() if k in keys}
+    tensors = tokenizer.pad(
+        batch_model_input, "longest", "left", return_tensors="pt"
+    ).to(device)
+    return tensors
+
+
+def _generate_response_text(
+    model: Union[PreTrainedModel, PeftModelForCausalLM],
+    batch_tensors: BatchEncoding,
+    temperature: float,
+    top_p: float,
+    max_new_tokens: int,
+    tokenizer: PreTrainedTokenizerFast
+) -> List[str]:
+    outputs = model.generate(
+        **batch_tensors,
+        temperature=temperature,
+        top_p=top_p,
+        max_new_tokens=max_new_tokens,
+        pad_token_id=tokenizer.pad_token_id
+    )
+    input_sequence_dim = batch_tensors.input_ids.shape[1]
+    output_ids = outputs[:, input_sequence_dim:].detach().cpu().numpy()
+    text = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    return text
+
+
+def _prepare_answer_labels(
+    generated_text: str,
+    mcq_answer_pattern: re.Pattern,
+    tokenizer: PreTrainedTokenizerFast,
+    label: List[Dict[str, str]]
+) -> Tuple[Set[str]]:
+    answer_matches = re.findall(mcq_answer_pattern, generated_text)
+    answer_set = set(map(lambda s: re.sub('[^A-Z]', '', s), answer_matches))
+    target = label[0]["content"].strip("\n")
+    y_set = set(target.split(","))
+    return answer_set, y_set
+
+
+def _calculate_metric_inputs(answer_set: Set[str], y_set: Set[str]) -> Tuple[int]:
+    if answer_set:
+        overlap = answer_set.intersection(y_set)
+        correct = len(overlap)  # true positives
+        incorrect = len(answer_set - overlap)  # false positives
+        missed = len(y_set - overlap)  # false negatives
+        exact = int(correct == len(y_set))
+    else:
+        correct = incorrect = exact = 0
+        missed = len(y_set)
+    return correct, incorrect, missed, exact
+
+
+def _calculate_metrics(counts: Dict[str, int]) -> Dict[str, float]:
+    metrics = {}
+    metrics["accuracy"] = counts["exact_match"] / counts["total_docs"]
+    precision_denom = counts["num_correct_responses"] + counts["num_incorrect_responses"]
+    precision = (counts["num_correct_responses"] / precision_denom) if precision_denom else 0.
+    metrics["precision"] = precision
+    recall_denom = counts["num_correct_responses"] + counts["num_missed_responses"]
+    recall = (counts["num_correct_responses"] / recall_denom) if recall_denom else 0.
+    metrics["recall"] = recall
+    f1_denom = precision + recall
+    metrics["f1"] = (2 * precision * recall / f1_denom) if f1_denom else 0.
+    return metrics
 
 
 def mcqa(
@@ -20,90 +106,71 @@ def mcqa(
     top_p: float = .9,
     inspect_responses_live: bool = False,
     return_all_outputs: bool = False,
-) -> Dict[str, float]:
-    eval_counts = defaultdict(int)
-    if not tokenizer.pad_token_id:
-        tokenizer.add_special_tokens({"pad_token": tokenizer.eos_token})
-    total_docs = len(dataset)
-    if not "seq_len" in dataset.features:
-        dataset = dataset.map(
-            lambda x: {"seq_len": len(x["input_ids"])},
-            desc="Counting tokens",
-        ).sort("seq_len", reverse=False)
-    batched_dataset = dataset.batch(batch_size)
-    if return_all_outputs:
-        generations = {}
-        num_batch_tokens = []
+) -> Dict[str, Any]:
+    
+    ## SETUP ##
     generation_input_keys = "input_ids", "attention_mask"
-    for batch_idx, batch in enumerate(tqdm(
-        batched_dataset,
-        desc="Running generation+eval",
-        disable=inspect_responses_live
-    )):
-        batch_model_input = {k: v for k, v in batch.items() if k in generation_input_keys}
-        batch_tensors = tokenizer.pad(
-            batch_model_input, "longest", "left", return_tensors="pt"
-        ).to(model.device)
+    progress_desc = "Running Generation + MCQ Evaluation"
+    if not tokenizer.pad_token_id:
+        # a padding token is needed to prepare homogenous batches
+        tokenizer.add_special_tokens({"pad_token": tokenizer.eos_token})
+    eval_counts = defaultdict(int)  # to hold metric inputs
+    eval_counts["total_docs"] += len(dataset)
+    if return_all_outputs:
+        # containers for full run details
+        generations = {}
+        num_batch_tokens = []    
+
+    ## DATASET PREP ##
+    batched_dataset = _prepare_dataset(dataset, batch_size)
+    batch_iterator = tqdm(batched_dataset, desc=progress_desc, disable=inspect_responses_live)
+    
+    for batch_idx, batch in enumerate(batch_iterator):
+        
+        ## INPUT COLLATION ##
+        batch_tensors = _prepare_batch(batch, generation_input_keys, tokenizer, model.device)
+        if not batch_idx:
+            print(type(batch_tensors))
         if return_all_outputs:
+            # keep track of how many total tokens are being passed in
+            # this can sometimes help with diagnosing memory issues etc.
             batch_dim = batch_tensors["input_ids"].shape
             num_batch_tokens.append(batch_dim[0] * batch_dim[1])
-        outputs = model.generate(
-            **batch_tensors,
-            temperature=temperature,
-            top_p=top_p,
-            max_new_tokens=max_new_tokens,
-            pad_token_id=tokenizer.pad_token_id
+        
+        ## INFERENCE ##
+        output_text_list = _generate_response_text(
+            model, batch_tensors, temperature, top_p, max_new_tokens, tokenizer
         )
-        output_ids = outputs.detach().cpu().numpy()
-        output_text_list = tokenizer.batch_decode(output_ids)
-        for i, (id_, str_, label) in enumerate(
-            zip(batch["doc_id"], output_text_list, batch["completion"])
-        ):
-            if answer_split_token is None:
-                input_text = tokenizer.decode(batch_tensors["input_ids"][i])
-                answer_split_idx = len(input_text)
-                answer = str_[answer_split_idx:]
-            else:
-                answer = str_.split(answer_split_token)[-1]
-            answer_matches = re.findall(mcq_answer_pattern, answer)
-            answer_set = set(map(lambda s: re.sub('[^A-Z]', '', s), answer_matches))
-            target = label[0]["content"].strip("\n")
-            y_set = set(target.split(","))
+        evaluation_data = zip(batch["doc_id"], output_text_list, batch["completion"])
+        
+        ## RAW SCORES ##
+        for i, (id_, generated_text, label) in enumerate(evaluation_data):
+            answer_set, y_set = _prepare_answer_labels(
+                generated_text, mcq_answer_pattern, tokenizer, label
+            )
             if inspect_responses_live:
-                print(f"DOC : {id_} ({batch_idx + i}/{total_docs})")
+                # prints out details and waits for confirmation before continuing
+                # allows you to read the model's answers as they're generated
+                print(f"DOC : {id_} ({batch_idx + i}/{eval_counts['total_docs']})")
                 print("LABEL:", ", ".join(y_set))
-                print("GENERATED TEXT :", answer)
+                print("GENERATED TEXT :", generated_text)
                 print("EXTRACTED ANSWER :", ", ".join(answer_set))
                 input("Press Enter to continue")
-            if answer_set:
-                overlap = answer_set.intersection(y_set)
-                num_correct_responses = len(overlap)  # true positives
-                num_incorrect_responses = len(answer_set - overlap)  # false positives
-                num_missed_responses = len(y_set - overlap)  # false negatives
-                exact_match = int(num_correct_responses == len(y_set))
-            else:
-                num_correct_responses = num_incorrect_responses = exact_match = 0
-                num_missed_responses = len(y_set)
-            eval_counts["num_correct_responses"] += num_correct_responses
-            eval_counts["num_incorrect_responses"] += num_incorrect_responses
-            eval_counts["num_missed_responses"] += num_missed_responses
-            eval_counts["exact_match"] += exact_match
+            metric_inputs = _calculate_metric_inputs(answer_set, y_set)
+            eval_counts["num_correct_responses"] += metric_inputs[0]
+            eval_counts["num_incorrect_responses"] += metric_inputs[1]
+            eval_counts["num_missed_responses"] += metric_inputs[2]
+            eval_counts["exact_match"] += metric_inputs[3]
             if return_all_outputs:
-                generations[id_] = str_
-    metrics = {}
-    metrics["accuracy"] = eval_counts["exact_match"] / len(dataset)
-    precision_denom = eval_counts["num_correct_responses"] + eval_counts["num_incorrect_responses"]
-    precision = (eval_counts["num_correct_responses"] / precision_denom) if precision_denom else 0.
-    metrics["precision"] = precision
-    recall_denom = eval_counts["num_correct_responses"] + eval_counts["num_missed_responses"]
-    recall = (eval_counts["num_correct_responses"] / recall_denom) if recall_denom else 0.
-    metrics["recall"] = recall
-    f1_denom = precision + recall
-    metrics["f1"] = (2 * precision * recall / f1_denom) if f1_denom else 0.
+                generations[id_] = generated_text
+    
+    ## METRICS ##
+    metrics = _calculate_metrics(eval_counts)
     ret = {"metrics": metrics}
     if return_all_outputs:
         ret["generations"] = generations
         ret["eval_counts"] = eval_counts
         ret["num_batch_tokens"] = num_batch_tokens
+    
     return ret
 
